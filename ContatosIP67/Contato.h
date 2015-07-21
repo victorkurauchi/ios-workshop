1f5b2ac3013f0d1bf129c344323862fb6c530866
Victor Kurauchi <victorkurauchi@gmail.com>
2015-07-20 22:45:53 -0300

    abstracting models

---
+++ ContatosIP67/Contato.h
@@ -0,0 +1,13 @@
+//
+//  Contato.h
+//  ContatosIP67
+//
+//  Created by ios5128 on 20/07/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contato : NSObject
+
+@end
---
+++ ContatosIP67/Contato.m
@@ -0,0 +1,13 @@
+//
+//  Contato.m
+//  ContatosIP67
+//
+//  Created by ios5128 on 20/07/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import "Contato.h"
+
+@implementation Contato
+
+@end
